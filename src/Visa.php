a09a39ffa3cd591f640180ab30643f3740711f0a
Derrick Obedgiu <derrickobedgiu@gmail.com>
2023-02-01 18:18:59 +0300

    Create Visa.php

---
+++ src/Visa.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Payline\EasyPay;
+
+Class Visa extends EasyPay{
+
+	public function requestVisaPayment($params){
+		try{
+			$visaDetails = [
+			"amount" =>$params['amount'],
+			"currency" =>$params['currency'],
+			"name" =>$params['name'],
+			"cardno" =>$params['cardno'],
+			"cvv" =>$params['cvv'],
+			"month" =>$params['month'],
+			"year" =>$params['year'],
+			"email" =>$params['email'],
+			"address" =>$params['address'],
+			"city" =>$params['city'],
+			"state" =>$params['state'],
+			"zip" =>$params['zip'],
+			"country" =>$params['country'],
+			"phone" =>$params['phone'],
+			"reference" =>$params['reference'],
+			"reason" =>$params['reason']
+		]
+			return $this->makeRequest("cardpayment",$visaDetails);
+		}
+		catch(\Exception $e){
+			return $e->getMessage;
+		}
+	}
+}
+?>
